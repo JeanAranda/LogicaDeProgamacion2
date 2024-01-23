4ba63386e4cb4bc57e6bee4cf993f904d41c6246
JeanAranda <jarandar2014@gmail.com>
2024-01-22 23:37:14 -0500

    Avance de la primera aula 2

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Juego del número secreto';
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Indica el número del 1 al 10';
+
+function intentoDeUsuario() {
+    alert('Click desde el boton de la funcion')
+}
