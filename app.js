6682450064aebb951bf9144d7cdb27de1cc38e0d
JeanAranda <jarandar2014@gmail.com>
2024-01-24 20:49:31 -0500

    funciones y manipulacion del doom

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 let numeroSecreto = generarNumeroSecreto();
+let intentos = 1;
+console.log(numeroSecreto);
 
 function asignarTexoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
@@ -8,16 +10,24 @@ function asignarTexoElemento(elemento, texto){
 
 function verificarIntento(){
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
-    console.log(numeroSecreto);
     if (numeroDeUsuario === numeroSecreto){
-        asignarTexoElemento('p','Acertaste el numero');
+        asignarTexoElemento('p',`Acertaste el numero en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+        //el usuario no acerto
     }else if(numeroDeUsuario > numeroSecreto){
         asignarTexoElemento('p', 'El numero secreto es menor');
     }else {
         asignarTexoElemento('p', 'El numero secreto es mayor')
     }
+    intentos++;
+    limpiarCaja();
     return;
 };
+
+function limpiarCaja(){
+    document.querySelector('#valorUsuario').value = '';
+    
+}
 
 function generarNumeroSecreto(){
     return Math.floor(Math.random()*10)+1;
